51c00691d84acf952a1568d4944fac1b178062be
sibasish <sibasishsahu195@gmail.com>
2024-12-06 14:29:11 +0530

    first commit

---
+++ src/script.js
@@ -0,0 +1,8 @@
+let input = document.getElementById("inputbox")
+let buttons = document.querySelector("button")
+let string = "";
+let arr = Array.from(buttons)
+arr.forEach(button => {
+    button.addEventListener('click', )
+})
+    
